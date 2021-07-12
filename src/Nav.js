fa8df136715d7bd368411cc158bea6f0428c4b83
annin-tofu <yuok5770@gmail.com>
2021-07-13 04:53:25 +0900

    nav bar is done

--- src/Nav.js
+++ src/Nav.js
@@ -6,12 +6,13 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <div className="nav">
+    <div className="nav nav__black">
+      {/* BEM */}
       <div className="nav__contents">
         {/* NEXTFIX LOGO */}
         <img
           className="nav__logo"
-          src="https://res.cloudinary.com/dhyagpwyl/image/upload/v1626102464/41f06ef73c50b9771360dc1a858750fd_lzskhw.jpg"
+          src="https://res.cloudinary.com/dhyagpwyl/image/upload/v1626119196/Nextfix_logo_vpneqm.png"
           //alt property is for SEO. add words to describe the image. here "nextfix" will the best. You can also leave it empty, but always alt="" needs to be included in img tag.
           alt="nextfix"
         />
