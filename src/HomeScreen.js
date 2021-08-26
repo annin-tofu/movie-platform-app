3a8ac9ef5242b398e6400c9d71c930239b7934cc
annin-tofu <yuok5770@gmail.com>
2021-08-27 00:00:59 +0900

    Row added

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -4,6 +4,8 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
@@ -15,6 +17,17 @@ function HomeScreen() {
       <Banner />
 
       {/* ROW */}
+      <Row 
+      title="NEXTFIX ORIGINALS"
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title="Trending Now"
+      fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated"
+      fetchUrl={requests.fetchTopRated}/>
+ 
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Row.css"
+
+function Row() {
+    return (
+        <div className="row">
+            
+        </div>
+    )
+}
+
+export default Row
