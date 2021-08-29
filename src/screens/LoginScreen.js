423494655101ead663a26c59c34ab1613b89ad93
annin-tofu <yuok5770@gmail.com>
2021-08-30 00:13:20 +0900

    loginScreen UI and styling done

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,6 +13,24 @@ function LoginScreen() {
         <button className="loginScreen__button">Sign In</button>
 
         <div className="loginScreen__gradient"></div>
+
+        <div className="loginScreen__body">
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button className="loginScreen__getStarted">GET STARTED</button>
+              </form>
+            </div>
+          </>
+        </div>
       </div>
     </div>
   );
