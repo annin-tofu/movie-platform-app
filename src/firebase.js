f708d0917d1fcdf94278307d0f96ef6601081d54
annin-tofu <yuok5770@gmail.com>
2021-09-03 15:12:17 +0900

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // setup firebase
+import firebase from "./firebase";
+// path must be "./firebase", not "firebase"!!
 
 const firebaseConfig = {
   apiKey: "AIzaSyAF1dlDj6Rf3dsRWAM4FDzFTIa4Yv_aUEo",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "304433646450",
   appId: "1:304433646450:web:e76bc25ed97e9713a02beb",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth }; // "explicit" export 03012200 . explicit export can have as many item as you want
+export default db; // "default" export 03012200. default export can only have one item.
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
+import { auth } from "../firebase";
 import "./SignupScreen.css";
 
 function SignupScreen() {
+  //!!! useRef !!!! // 03012500
+  const emailRef = useRef(null); // useRef: is like a "big finger" is pointing at a HTML element 03012300
+  const passwordRef = useRef(null);
+
+  //AUTHENTICATION 03011700
+  const register = (e) => {
+    e.preventDefault(); // e.preventDefault: dont want to do typical refresh.(it, by default, refreshes... so you want to prevent that default behavior) 03011700
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value, // 03012500
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      }); // 03012500
+  };
+  const signIn = (e) => {
+    e.preventDefault();
+  }; //e.preventDefault: dont want to do typical refresh.(it, by default, refreshes... so you want to prevent that default behavior) 03011700
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
-        <button type="submit"> Sign In </button>
+        <input
+          ref={emailRef} // 03012500
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          ref={passwordRef} // 03012500
+          placeholder="Password"
+          type="password"
+        />
+        <button
+          type="submit"
+          onClick={signIn} //03011700
+        >
+          Sign In
+        </button>
         <h4>
           {/* When to use span tag? 03011400 */}
           {/* this way, "New to Nextfix" is gray, but "Sign Up now" is white and highlighted */}
           <span className="signupScreen__gray">New to Nextfix?</span>
-          <span className="signupScreen__link">Sign Up now.</span>
+          <span
+            className="signupScreen__link"
+            onClick={register} //03011700
+          >
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
